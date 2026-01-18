cd6caa3fd661f0fa66c09236ecfb05229bae57d7
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-01-18 16:05:09 +0100

    hmmm

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -13,3 +13,23 @@ print("Books in library:", books_in_library)
 # Retrieve the librarian for a library
 librarian = Librarian.objects.get(library=library)
 print("Librarian:", librarian.name)
+
+
+from relationship_app.models import Author, Book, Library, Librarian
+
+# Query all books by a specific author
+author_name = "Chinua Achebe"
+author = Author.objects.get(name=author_name)
+books_by_author = Book.objects.filter(author=author)
+print(books_by_author)
+
+# List all books in a library
+library_name = "Central Library"
+library = Library.objects.get(name=library_name)
+books_in_library = library.books.all()
+print(books_in_library)
+
+# Retrieve the librarian for a library
+librarian = Librarian.objects.get(library=library)
+print(librarian)
+
