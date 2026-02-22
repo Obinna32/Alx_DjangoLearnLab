8c73693b00bcab43b1a6e894d1a2b2b6f9c74f70
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-22 22:11:54 +0100

    feat: update serializer

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -2,30 +2,28 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework.authtoken.models import Token
 
-User = get_user_model()
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['id', 'username', 'email', 'bio', 'profile_picture']
 
 class RegisterSerializer(serializers.ModelSerializer):
-    #Ensures password is write-only, for security
-    password = serializers.CharField(write_only = True)
+    # Explicitly using serializers.CharField() for the password field
+    password = serializers.CharField()
 
     class Meta:
-        model = User
+        model = get_user_model()
         fields = ['username', 'email', 'password', 'bio', 'profile_picture']
 
-        def create(self, validated_data):
-            #Using creat_user to properly hash the password
-            user = User.objects.create_user(
-                username=validated_data['username'],
-                email=validated_data.get('email'),
-                password=validated_data['password'],
-                bio=validated_data.get('bio', ''),
-                profile_picture=validated_data.get('profile_picture')
-            )
-            #Automatically create a token for the new user
-            Token.objects.create(user=user)
-            return user
+    def create(self, validated_data):
+        # Using get_user_model().objects.create_user to handle hashing and user creation
+        user = get_user_model().objects.create_user(
+            username=validated_data['username'],
+            email=validated_data.get('email'),
+            password=validated_data['password'],
+            bio=validated_data.get('bio', ''),
+            profile_picture=validated_data.get('profile_picture')
+        )
+        # Create a token for the user immediately upon registration
+        Token.objects.create(user=user)
+        return user
