b47c9c6f1e5b025323dd0fd097f372d99a458a21
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-22 22:01:46 +0100

    feat: define app serializer, urls, views

--- social_media_api/accounts/models.py
+++ social_media_api/accounts/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    bio = models.TextField(blank=True, null=True)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    #symmetrical = False allows "User A follows User B" without forcing "User B to follow A"
+    followers = models.ManyToManyField('self', symmetrical=False, related_name='following', blank=True)
+
+    def __str__(self):
+        return self.username
--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,3 +1,38 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework import status, generics, permission
+from .serializers import RegisterSerializer, UserSerializer
 from django.shortcuts import render
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            token = Token.objects.get(user=user)
+            return Response({
+                'token': token.key,
+                'user': UserSerializer(user).data
+            }, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class LoginView(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        response = super(LoginView, self).post(request, *args, **kwargs)
+        token = Token.objects.get(key=response.data['token'])
+        return Response({
+            'token': token.key,
+            'user_id': token.user_id,
+            'username': token.user.username
+        })
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permission.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
