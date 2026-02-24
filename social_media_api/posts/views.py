c5c11b943cd17ba32ba87fc2b4a642715e72c58d
Obinna Ekwealor <obinnaekwealor534@gmail.com>
2026-02-24 12:24:34 +0100

    ..

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -34,5 +34,5 @@ class FeedView(generics.ListAPIView):
 
     def get_queryset(self):
         user = self.request.user
-        following_users = user.followimg.all()
+        following_users = user.following.all()
         return Post.objects.filter(author__in=following_users).order_by('-created_at')
